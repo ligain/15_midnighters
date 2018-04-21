3409e6377a4790fc5f7e19c2dde7532139aab3ba
ligain <khokhlov86@gmail.com>
2018-04-21 11:27:22 +0300

    fixed requirements remastered load_attempts function deleted get_pagination_range func

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -6,17 +6,6 @@ from datetime import datetime, time
 API_URL = 'https://devman.org/api/challenges/solution_attempts'
 
 
-def get_pagination_range(api_url):
-    api_response = requests.get(api_url)
-
-    if not api_response.ok:
-        return
-    json_response = api_response.json()
-    start_page = json_response.get('page')
-    end_page = json_response.get('number_of_pages')
-    return start_page, end_page
-
-
 def get_paginated_page(api_url, page_num, pagination_slug='page'):
     api_response = requests.get(
         api_url,
@@ -30,13 +19,26 @@ def get_paginated_page(api_url, page_num, pagination_slug='page'):
     return json_response.get('records', [])
 
 
-def load_attempts(api_url, start_page, end_page):
-    for page in range(start_page, end_page):
+def load_attempts(api_url, max_api_attempts=1000):
+    page = 1
+    failed_api_attempts = 0
+    while failed_api_attempts < max_api_attempts:
+        api_response = requests.get(api_url)
+
+        if not api_response.ok:
+            failed_api_attempts += 1
+            continue
+
+        json_response = api_response.json()
         yield from get_paginated_page(api_url, page)
+
+        page += 1
+        if page >= json_response.get('number_of_pages'):
+            break
 
 
 def is_midnighter(user_timestamp: float, user_timezone: str,
-                  night_start=time(hour=0), night_end=time(hour=6)):
+                  night_start_hour=0, night_end_hour=6):
 
     user_tz = pytz.timezone(user_timezone)
 
@@ -44,28 +46,15 @@ def is_midnighter(user_timestamp: float, user_timezone: str,
         user_timestamp).replace(tzinfo=pytz.utc)
 
     user_localized_date = user_date_utc.astimezone(user_tz)
-
-    extracted_date = user_localized_date.date()
-
-    localized_night_start = datetime.combine(
-        extracted_date,
-        night_start
-    ).replace(tzinfo=user_tz)
-
-    localized_night_end = datetime.combine(
-        extracted_date,
-        night_end
-    ).replace(tzinfo=user_tz)
-
-    if localized_night_start < user_localized_date < localized_night_end:
+    if night_start_hour <= user_localized_date.hour <= night_end_hour:
         return True
     return False
 
 
 def get_midnighters(users_attempts):
     for attempt in users_attempts:
-        user_timestamp = float(attempt.get('timestamp', 0.0))
-        user_timezone = attempt.get('timezone', '')
+        user_timestamp = attempt.get('timestamp')
+        user_timezone = attempt.get('timezone')
         if user_timestamp and user_timezone and is_midnighter(
                 user_timestamp, user_timezone):
             yield attempt.get('username', '')
@@ -78,13 +67,8 @@ def print_midnighters(midnighters):
 
 
 if __name__ == '__main__':
-    pagination_range = get_pagination_range(API_URL)
 
-    if pagination_range is None:
-        exit('Error loading json from the API endpoint')
-
-    users_attempts = load_attempts(API_URL, *pagination_range)
+    users_attempts = load_attempts(API_URL)
 
     midnighters = set(get_midnighters(users_attempts))
     print_midnighters(midnighters)
-
